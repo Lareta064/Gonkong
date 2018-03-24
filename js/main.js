4174650469407ad905db95bb844839f8dd24df3c
Lareta <lareta1@yandex.ru>
2018-03-24 18:02:44 +0300

    commit-12

--- js/main.js
+++ js/main.js
@@ -84,3 +84,27 @@ $('.overlay_popup').click(function() { // Обрабатываем клик по
 });
 
 });
+
+ $(document).ready(function(){
+  // появление/затухание кнопки #back-top
+  $(function (){
+    // прячем кнопку #back-top
+    $("#back-top").hide();
+  
+    $(window).scroll(function (){
+      if ($(this).scrollTop() > 100){
+        $("#back-top").fadeIn();
+      } else{
+        $("#back-top").fadeOut();
+      }
+    });
+
+    // при клике на ссылку плавно поднимаемся вверх
+    $("#back-top a").click(function (){
+      $("body,html").animate({
+        scrollTop:0
+      }, 800);
+      return false;
+    });
+  });
+});
